a1fd7f32ccb58f56a12165bae732386af280c93b
ronimizy <ronimizy@outlook.com>
2023-09-12 20:49:45 +0300

    fix: submission mapping

--- src/Presentation/Itmo.Dev.Asap.Core.Presentation.Grpc.Mapping/SubmissionWorkflowMapper.cs
+++ src/Presentation/Itmo.Dev.Asap.Core.Presentation.Grpc.Mapping/SubmissionWorkflowMapper.cs
@@ -1,5 +1,6 @@
 using Google.Protobuf.WellKnownTypes;
 using Itmo.Dev.Asap.Core.Application.Dto.Submissions;
+using Itmo.Dev.Asap.Core.Models;
 using Itmo.Dev.Asap.Core.SubmissionWorkflow;
 using Riok.Mapperly.Abstractions;
 
@@ -9,6 +10,9 @@ namespace Itmo.Dev.Asap.Core.Presentation.Grpc.Mapping;
 internal static partial class SubmissionWorkflowMapper
 {
     public static partial UpdatedResponse MapFrom(this SubmissionUpdateResult result);
+
+    [MapProperty(nameof(SubmissionRateDto.Id), nameof(SubmissionRate.SubmissionId))]
+    private static partial SubmissionRate MapToSubmissionRate(SubmissionRateDto rate);
 
     private static Timestamp ToTimestamp(DateTime dateTime)
         => Timestamp.FromDateTime(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc));
