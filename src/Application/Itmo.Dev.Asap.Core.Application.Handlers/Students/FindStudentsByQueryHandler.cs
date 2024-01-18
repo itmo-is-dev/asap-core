051c648ccf9a7d34a5404968846cfd8f1884c7b1
ronimizy <ronimizy@outlook.com>
2024-01-18 21:10:31 +0300

    chore: added student query logging

--- src/Application/Itmo.Dev.Asap.Core.Application.Handlers/Students/FindStudentsByQueryHandler.cs
+++ src/Application/Itmo.Dev.Asap.Core.Application.Handlers/Students/FindStudentsByQueryHandler.cs
@@ -3,6 +3,7 @@ using Itmo.Dev.Asap.Core.Application.DataAccess.Queries;
 using Itmo.Dev.Asap.Core.Application.Dto.Students;
 using Itmo.Dev.Asap.Core.Mapping;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using static Itmo.Dev.Asap.Core.Application.Contracts.Students.Queries.FindStudentsByQuery;
 
 namespace Itmo.Dev.Asap.Core.Application.Handlers.Students;
@@ -10,10 +11,12 @@ namespace Itmo.Dev.Asap.Core.Application.Handlers.Students;
 internal class FindStudentsByQueryHandler : IRequestHandler<Query, Response>
 {
     private readonly IPersistenceContext _context;
+    private readonly ILogger<FindStudentsByQueryHandler> _logger;
 
-    public FindStudentsByQueryHandler(IPersistenceContext context)
+    public FindStudentsByQueryHandler(IPersistenceContext context, ILogger<FindStudentsByQueryHandler> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
@@ -30,6 +33,11 @@ internal class FindStudentsByQueryHandler : IRequestHandler<Query, Response>
             .QueryAsync(query, cancellationToken)
             .Select(x => x.ToDto())
             .ToArrayAsync(cancellationToken);
+
+        _logger.LogTrace(
+            "Finished students query = {Query}, found {Count} records",
+            query,
+            dto.Length);
 
         return new Response(dto);
     }
--- src/Infrastructure/Itmo.Dev.Asap.Core.DataAccess/Configuration/DbNamesConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Itmo.Dev.Asap.Core.DataAccess.Configuration;
-
-public class DbNamesConfiguration
-{
-    public string ApplicationDbName { get; init; } = string.Empty;
-
-    public string IdentityDbName { get; init; } = string.Empty;
-}
